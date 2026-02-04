bf127343f5ea7dfd030af1092923b15e3d5b443e
JerryKhw <jerry@jerrykhw.dev>
2026-02-04 20:26:35 +0900

    update: 관심주제 선택 이미지 수정

--- lib/asset/webp.dart
+++ lib/asset/webp.dart
@@ -3,8 +3,28 @@ part of 'index.dart';
 class WebpImage {
   WebpImage._();
 
-  static const String categoryAuth = 'assets/webp/category_auth.webp';
   static const String categoryBangtong = 'assets/webp/category_bangtong.webp';
+  static const String categoryChild = 'assets/webp/category_child.webp';
+  static const String categoryDigital = 'assets/webp/category_digital.webp';
+  static const String categoryDiplomacy = 'assets/webp/category_diplomacy.webp';
   static const String categoryEconomy = 'assets/webp/category_economy.webp';
+  static const String categoryEducation = 'assets/webp/category_education.webp';
+  static const String categoryEnvironment =
+      'assets/webp/category_environment.webp';
+  static const String categoryFamily = 'assets/webp/category_family.webp';
+  static const String categoryFemale = 'assets/webp/category_female.webp';
+  static const String categoryLabor = 'assets/webp/category_labor.webp';
+  static const String categoryLaw = 'assets/webp/category_law.webp';
+  static const String categoryMedical = 'assets/webp/category_medical.webp';
+  static const String categoryRealEstate =
+      'assets/webp/category_real_estate.webp';
+  static const String categorySecurity = 'assets/webp/category_security.webp';
+  static const String categorySexCrime = 'assets/webp/category_sex_crime.webp';
+  static const String categoryTraffic = 'assets/webp/category_traffic.webp';
+  static const String categoryWelfare = 'assets/webp/category_welfare.webp';
   static const String loading = 'assets/webp/loading.webp';
+  static const String registerOnboard1 = 'assets/webp/register_onboard1.webp';
+  static const String registerOnboard2 = 'assets/webp/register_onboard2.webp';
+  static const String registerOnboard3 = 'assets/webp/register_onboard3.webp';
+  static const String registerOnboard4 = 'assets/webp/register_onboard4.webp';
 }
--- test/webp_test.dart
+++ test/webp_test.dart
@@ -5,9 +5,27 @@ import 'package:mypoly/asset/index.dart';
 
 void main() {
   test('webp assets test', () {
-    expect(File(WebpImage.categoryAuth).existsSync(), isTrue);
     expect(File(WebpImage.categoryBangtong).existsSync(), isTrue);
+    expect(File(WebpImage.categoryChild).existsSync(), isTrue);
+    expect(File(WebpImage.categoryDigital).existsSync(), isTrue);
+    expect(File(WebpImage.categoryDiplomacy).existsSync(), isTrue);
     expect(File(WebpImage.categoryEconomy).existsSync(), isTrue);
+    expect(File(WebpImage.categoryEducation).existsSync(), isTrue);
+    expect(File(WebpImage.categoryEnvironment).existsSync(), isTrue);
+    expect(File(WebpImage.categoryFamily).existsSync(), isTrue);
+    expect(File(WebpImage.categoryFemale).existsSync(), isTrue);
+    expect(File(WebpImage.categoryLabor).existsSync(), isTrue);
+    expect(File(WebpImage.categoryLaw).existsSync(), isTrue);
+    expect(File(WebpImage.categoryMedical).existsSync(), isTrue);
+    expect(File(WebpImage.categoryRealEstate).existsSync(), isTrue);
+    expect(File(WebpImage.categorySecurity).existsSync(), isTrue);
+    expect(File(WebpImage.categorySexCrime).existsSync(), isTrue);
+    expect(File(WebpImage.categoryTraffic).existsSync(), isTrue);
+    expect(File(WebpImage.categoryWelfare).existsSync(), isTrue);
     expect(File(WebpImage.loading).existsSync(), isTrue);
+    expect(File(WebpImage.registerOnboard1).existsSync(), isTrue);
+    expect(File(WebpImage.registerOnboard2).existsSync(), isTrue);
+    expect(File(WebpImage.registerOnboard3).existsSync(), isTrue);
+    expect(File(WebpImage.registerOnboard4).existsSync(), isTrue);
   });
 }
