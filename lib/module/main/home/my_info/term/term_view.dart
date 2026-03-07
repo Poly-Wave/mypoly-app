c79fe1ba9dd01597c71bd53e097a899bb343a0c1
JerryKhw <jerry@jerrykhw.dev>
2026-03-07 19:39:01 +0900

    feat: 약관

--- lib/module/main/home/my_info/term/term_view.dart
+++ lib/module/main/home/my_info/term/term_view.dart
@@ -1,6 +1,11 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:mypoly/module/main/home/my_info/my_info_view.dart';
+import 'package:mypoly/provider/app_provider.dart';
+import 'package:mypoly/provider/router_provider.dart';
+import 'package:mypoly/widget/index.dart';
 
 @RoutePage()
 class TermView extends HookConsumerWidget {
@@ -8,6 +13,35 @@ class TermView extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold();
+    final terms = ref.watch(appTermsProvider);
+
+    return Scaffold(
+      appBar: MPAppbar(context, text: "약관"),
+      body: Column(
+        crossAxisAlignment: .stretch,
+        children: [
+          Expanded(
+            child: MPSingleScroll(
+              child: Padding(
+                padding: .symmetric(horizontal: 20.w),
+                child: MPSafeColumn(
+                  bottom: true,
+                  crossAxisAlignment: .stretch,
+                  children: terms
+                      .map(
+                        (term) => MyInfoGroupItem(
+                          term.title,
+                          onTap: () =>
+                              context.pushRoute(TermDetailRoute(data: term)),
+                        ),
+                      )
+                      .toList(),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
