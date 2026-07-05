d2093f1c3060f1c1b10e008cfe7879ffcbf9ff16
JerryKhw <jerry@jerrykhw.dev>
2026-07-05 10:47:26 +0900

    fix: 진행단계 텍스트 수정

--- lib/enum/stage.dart
+++ lib/enum/stage.dart
@@ -2,13 +2,13 @@ import 'package:json_annotation/json_annotation.dart';
 
 enum Stage {
   @JsonKey(name: "RECEIVED")
-  received(text: "RECEIVED", value: "RECEIVED"),
+  received(text: "RECEIVED", value: "접수"),
   @JsonKey(name: "REVIEW")
-  review(text: "REVIEW", value: "REVIEW"),
+  review(text: "REVIEW", value: "심사"),
   @JsonKey(name: "DECISION")
-  decision(text: "DECISION", value: "DECISION"),
+  decision(text: "DECISION", value: "의결"),
   @JsonKey(name: "COMPLETED")
-  completed(text: "COMPLETED", value: "COMPLETED");
+  completed(text: "COMPLETED", value: "완료");
 
   final String text;
   final String value;
