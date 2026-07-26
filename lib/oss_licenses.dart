48505b9b616909b8be3cba7cf6eda440b2630a92
JerryKhw <jerry@jerrykhw.dev>
2026-07-26 22:33:26 +0900

    release: 0.1.0+17

--- lib/oss_licenses.dart
+++ lib/oss_licenses.dart
@@ -44867,12 +44867,12 @@ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.''',
   );
 
-/// mypoly 0.1.0+16
+/// mypoly 0.1.0+17
 const _mypoly = Package(
     name: 'mypoly',
     description: 'MyPOLY',
     authors: [],
-    version: '0.1.0+16',
+    version: '0.1.0+17',
     spdxIdentifiers: [],
     isMarkdown: false,
     isSdk: false,
